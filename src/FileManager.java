9844a26445c8b5c4d42d9e7a7c7ab104a826182e
Bassam Husam <husamb@sheridancollege.ca>
2025-10-14 20:54:20 -0400

    Initial commit with FileManager.java by Bassam Husam

---
+++ src/FileManager.java
@@ -0,0 +1,17 @@
+package sheridan.husamb;
+
+
+public class FileManager {
+
+    public static void main(String[] args) {
+        System.out.println("Cross-platform FileManager running...");
+    }
+
+    public void createFile(String fileName) {
+        System.out.println("Creating file: " + fileName);
+    }
+
+    public void deleteFile(String fileName) {
+        System.out.println("Deleting file: " + fileName);
+    }
+}
